a347a4cf4638a053023962c5ce670be3f350912e
Yank <saskslay@gmail.com>
2015-07-21 14:30:12 +0100

    Hotfix
    Fixed code typo

--- sys/lua/reborn/commands.lua
+++ sys/lua/reborn/commands.lua
@@ -5,7 +5,7 @@ reb.ABOUT.commands = {
 	{"heroes", level = 0, desc = "Heroes menu", func = "cl.popHeroes"};
 	{"myheroes", level = 0, desc = "My heroes menu", func = "cl.popMyHeroes"};
 	{"clearheroes", level = 0, desc = "Reset heroes", func = "cl.delHeroes"};
-	{"getstats", "whatstats", level = 0, desc = "Show player stats", func = "cl.getStats"}
+	{"getstats", "whatstats", level = 0, desc = "Show player stats", func = "cl.getStats"};
 	{"reset", level = 0, desc = "Reset stats", concat = 1, func = "cl.reset"};
 	{"shop", level = 0, desc = "Shop", func = "cl.popShop"};
 	{"inventory", level = 0, desc = "Inventory", func = "cl.popInv"};
